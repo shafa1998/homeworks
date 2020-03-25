48b580fd41933f6f5b361a4ca49e17a3c360507d
Shafa <shafa.ahmadova1105@gmail.com>
2020-03-25 15:53:51 +0400

    --

--- src/main/java/homework8/Main.java
+++ src/main/java/homework8/Main.java
@@ -1,5 +1,8 @@
 package homework8;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
@@ -13,6 +16,24 @@ public class Main {
         Man man= new Man("Nurlan","Babayev",1980);
         Woman woman = new Woman("Sevinc", "Babayeva",1985);
         Family family = new Family(man,woman);
+        Pet dog2=new Dog("bethoven",5,20);
+        Pet pet = new Pet() {
+            @Override
+            public void respond() {
+                System.out.println("Hello");
+            }
+        };
+        Set<Pet> pets = new HashSet<>();
+        pets.add(dog2);
+        family.setPet(pets);
         System.out.println(family);
+        System.out.println(dog2);
+        System.out.println(pet);
+        pet.foul();
+        dog2.foul();
+        pet.respond();
+        dog2.respond();
+        man.greet();
+        woman.greet();
+        }
     }
-}
